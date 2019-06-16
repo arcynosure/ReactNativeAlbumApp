89e4dfc686121f9971083ce3058478a278ac9adc
arcynosure <arcynosure@gmail.com>
2019-06-16 20:55:42 +0530

    Front end 1

---
+++ index.js
@@ -0,0 +1,19 @@
+//import libraries
+
+import React from 'react';
+import { AppRegistry, View }  from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+//Create component
+
+const App = () => (
+    <View>
+        <Header headerText = { 'Youu' } />
+        <AlbumList />
+    </View>
+);
+
+//Render
+
+AppRegistry.registerComponent('youu', () => App);
